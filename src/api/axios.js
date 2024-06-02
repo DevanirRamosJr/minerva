3de662c50586847354f81dd26e7ec065cb785a78
DevanirRamosJr <junior472399@gmail.com>
2024-06-02 19:53:30 -0300

    feat: uptades api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    // baseURL: "https://minerva-world.onrender.com/", // Replace with your FastAPI base URL
-    baseURL: "http://localhost:5005",
+    baseURL: "https://minerva-world.onrender.com/",
+    //baseURL: "http://localhost:5005",
     headers: {
         "Content-Type": "application/json",
     },
